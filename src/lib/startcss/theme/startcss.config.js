edc9f5c12b4c9e8053de8cd1dcb7403110931d85
verdulife <verdu@live.com>
2023-02-07 20:23:46 +0100

    testing breackpoints

--- src/lib/startcss/theme/startcss.config.js
+++ src/lib/startcss/theme/startcss.config.js
@@ -1,20 +1,29 @@
-import { generatePalettes } from "./theme/generatePalettes.js";
+import { generatePalettes } from "./generatePalettes.js";
 
 // Add or modify colors as you like. Names will match definitions.
 // Defined color will be accesible as var(--your-color).
-// Autogenerate palettes from 100 to 900 as a CSS custom property.
 // Ex. var(--accent-100);
 
+// Autogenerate palettes from 100 to 900 as a CSS custom property.
 // All colors and palettes generate a HSL value custom property for opacity aplication.
 // Ex. hsl(var(--accent-100-hsl), 0.5);
 const colors = {
-  neutral: "#fafafa",
+  base: "#fafafa",
   accent: "#FFD55E",
   alt: "#041A40",
   info: "#1E4BCA",
   success: "#4BB543",
   warning: "#FF7900",
   error: "#CB0B44",
+}
+
+// Choose good contrast colors for texts
+// Defined color will auto add a "t-" prefix and accesible as var(--t-your-color).
+// Undefined colors will use base as default
+// Ex. var(--t-accent);
+const textColors = {
+  base: "#1a1a1a",
+  alt: "#fafafa",
 }
 
 generatePalettes(colors);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,26 +2,38 @@
 	import Image from '$lib/components/Image.svelte';
 </script>
 
-<section class="col acenter xfill">
-	<article class="row col@sm xfill">
+<section class="col acenter wfull">
+	<article class="col wfull">
 		<picture>
 			<Image src="/mobile.png" width="100px" height="100px" alt="Verdu" />
 		</picture>
 
 		<h1>Lorem ipsum</h1>
-		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam sit quam eligendi incidunt,
-			necessitatibus blanditiis illo nemo dicta accusamus alias sequi, quisquam excepturi ipsa
-			aspernatur porro repellendus, explicabo error placeat.
-		</p>
 
-		<button>BUTTON</button>
+		<footer class="col row@lg wfull">
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam sit quam eligendi incidunt,
+				necessitatibus blanditiis illo nemo dicta accusamus alias sequi, quisquam excepturi ipsa
+				aspernatur porro repellendus, explicabo error placeat.
+			</p>
+
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam sit quam eligendi incidunt,
+				necessitatibus blanditiis illo nemo dicta accusamus alias sequi, quisquam excepturi ipsa
+				aspernatur porro repellendus, explicabo error placeat.
+			</p>
+		</footer>
+
+		<aside class="col row@lg wfull">
+			<button class="wfull w1/3@md">BUTTON</button>
+			<button class="wfull w1/3@md">BUTTON 2</button>
+		</aside>
 	</article>
 </section>
 
 <style lang="postcss">
 	article {
-		max-width: 900px;
+		max-width: var(--lg);
 		gap: 2em;
 		padding: 2em;
 
@@ -36,10 +48,18 @@
 			font-weight: 551;
 		}
 
-		& button {
-			background-color: var(--accent);
-			border-radius: 0.5em;
-			padding: 1em 2em;
+		& footer {
+			gap: 2em;
+		}
+
+		& aside {
+			gap: 2em;
+
+			& button {
+				background-color: var(--accent);
+				border-radius: 0.5em;
+				padding: 1em 2em;
+			}
 		}
 	}
 </style>
