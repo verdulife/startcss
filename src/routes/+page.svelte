41538861c913132d1eb57e947b01aa426298aab9
verdulife <verdu@live.com>
2023-02-07 14:13:08 +0100

    testing breackpoints

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section class="col acenter xfill">
-	<article class="col xfill">
+	<article class="row sm:col xfill">
 		<picture>
 			<Image src="/mobile.png" width="100px" height="100px" alt="Verdu" />
 		</picture>
