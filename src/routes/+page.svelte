0e56971d53794adc4a3b5a86b39cd4c69531e8c8
verdulife <verdu@live.com>
2023-02-07 22:35:10 +0100

    componetize css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,63 +3,21 @@
 </script>
 
 <section class="col acenter wfull">
-	<article class="col wfull">
-		<picture>
-			<Image src="/mobile.png" width="100px" height="100px" alt="Verdu" />
-		</picture>
-
-		<h1>Lorem ipsum</h1>
-
-		<footer class="col row@lg wfull">
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam sit quam eligendi incidunt,
-				necessitatibus blanditiis illo nemo dicta accusamus alias sequi, quisquam excepturi ipsa
-				aspernatur porro repellendus, explicabo error placeat.
-			</p>
-
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam sit quam eligendi incidunt,
-				necessitatibus blanditiis illo nemo dicta accusamus alias sequi, quisquam excepturi ipsa
-				aspernatur porro repellendus, explicabo error placeat.
-			</p>
-		</footer>
-
-		<aside class="col row@lg wfull">
-			<button class="wfull w1/3@md">BUTTON</button>
-			<button class="wfull w1/3@md">BUTTON 2</button>
-		</aside>
-	</article>
+	<button>Button</button>
+	<a class="btn">Anchor as button</a>
+	<input type="text" placeholder="Input..." />
+	<h1>Heading <strong>h1</strong></h1>
+	<h2>Heading <strong>h2</strong></h2>
+	<h3>Heading <strong>h3</strong></h3>
+	<h4>Heading <strong>h4</strong></h4>
+	<h5>Heading <strong>h5</strong></h5>
+	<h6>Heading <strong>h6</strong></h6>
+	<p>Lorem ipsum dolor sit amet <b>consectetur</b> adipisicing elit.</p>
 </section>
 
 <style lang="postcss">
-	article {
-		max-width: var(--lg);
+	section {
 		gap: 2em;
-		padding: 2em;
-
-		& picture {
-			border-radius: 50%;
-			border: 2px solid var(--neutral-400);
-			overflow: hidden;
-		}
-
-		& h1 {
-			color: var(--neutral-700);
-			font-weight: 551;
-		}
-
-		& footer {
-			gap: 2em;
-		}
-
-		& aside {
-			gap: 2em;
-
-			& button {
-				background-color: var(--accent);
-				border-radius: 0.5em;
-				padding: 1em 2em;
-			}
-		}
+		padding: 4em;
 	}
 </style>
