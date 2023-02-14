69f0726bd69bdb84b98158cdfe7095d3e9a921f7
verdulife <verdu@live.com>
2023-02-14 23:28:32 +0100

    postcss each

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 import postcssPresetEnv from 'postcss-preset-env';
 import postcssFor from 'postcss-for';
+import postcssEach from 'postcss-each';
 
 const config = {
 	plugins: [
 		postcssFor(),
+		postcssEach(),
 		postcssPresetEnv({
 			features: {
 				'nesting-rules': true,
